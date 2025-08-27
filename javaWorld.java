6eacc0c28b594e1180c39c790eb4bd08ed4670c0
Mdrizwan <alleducation4200@gmail.com>
2025-08-28 01:56:22 +0800

    new Update javaWorld.java

--- javaWorld.java
+++ javaWorld.java
@@ -1,5 +1,7 @@
 public class javaWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+         System.out.println("Hello, World!2222222222");
     }
+
 }
